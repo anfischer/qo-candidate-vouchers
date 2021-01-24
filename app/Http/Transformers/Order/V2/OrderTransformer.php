9464b41862b99a0753cec16b1a2924485a13192e
Andreas Fischer <andreas@season.dk>
2021-01-24 11:48:40 +0100

    feat: getting an order via v2 transforms its vouchers into an array of ids

--- app/Http/Transformers/Order/V2/OrderTransformer.php
+++ app/Http/Transformers/Order/V2/OrderTransformer.php
@@ -10,9 +10,19 @@ final class OrderTransformer extends TransformerAbstract
 {
     public function transform(Order $order): array
     {
+        // If the order has any vouchers on its belongs to many relation
+        // the ids of the vouchers can be returned as is.
+        // Otherwise we wrap the old, API v1 voucher belongs to relation, in an array an return it to
+        // match the API v2 response.
+        if ($order->vouchers->count() > 0) {
+            $vouchers = $order->vouchers->pluck('id')->toArray();
+        } else {
+            $vouchers = [optional($order->voucher)->id];
+        }
+
         return [
             'id' => (int) $order->id,
-            'vouchers' => [$order->voucher_id],
+            'vouchers' => $vouchers,
             'total' => (int) $order->total,
         ];
     }
